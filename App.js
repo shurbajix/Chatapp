f6d22bf6e39196b048d0397a4abd1ce21e6b363c
SONY <{ID}+{username}@users.noreply.github.com>
2024-08-04 23:10:38 +0300

    add new line

--- App.js
+++ App.js
@@ -1,10 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ImageBackground } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <ImageBackground
+        style={styles.imageBackground}
+        source={{ uri: 'https://media.macphun.com/img/uploads/customer/how-to/608/15542038745ca344e267fb80.28757312.jpg?q=85&w=1340' }}
+        resizeMode="cover"
+      >
+        <View style={styles.overlay}>
+          <Text style={styles.text}>Welcome With React!</Text>
+        </View>
+      </ImageBackground>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +21,26 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+  },
+  imageBackground: {
+    flex: 1,
+    width: '100%',
+    height: '100%',
     justifyContent: 'center',
+    alignItems: 'center',
+  },
+  overlay: {
+    width: '80%',
+    height: '50%',
+    backgroundColor: 'rgba(0,0,0,0.5)', // Semi-transparent overlay
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 20,
+  },
+  text: {
+    color: 'white',
+    fontSize: 30,
+    
+    
   },
 });
