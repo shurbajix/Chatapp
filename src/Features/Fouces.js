1fa8a51cdad87b116a7934b5892de061c9b376a5
SONY <{ID}+{username}@users.noreply.github.com>
2024-09-22 11:08:14 +0300

    add new folders

--- src/Features/Fouces.js
+++ src/Features/Fouces.js
@@ -1,19 +1,26 @@
 import React from 'react';
-import {View, Text ,StyleSheet} from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 import { colors } from '../utils/colors';
 import { TextInput } from 'react-native-paper';
-export const Focus = () => (
-<View style={styles.container}>
-    
-    <TextInput label="Fouce Item" />
-</View>
-)
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
-const styles= StyleSheet.create({
-    container:{ 
-        flex: 1,
-    },
-    text:{
-        color: colors.white,
-    },
-})
+export const Focus = () => {
+  const insets = useSafeAreaInsets();
+
+  return (
+    <View style={[styles.container, { paddingTop: insets.top }]}>
+      <Text style={styles.text}>
+        welcomes
+      </Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: { 
+    flex: 1,
+  },
+  text: {
+    color: colors.white,
+  },
+});
